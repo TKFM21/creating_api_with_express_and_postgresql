4ae3de70192e408f179a17f97eee866785d664d5
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-10-26 23:33:56 +0900

    express env setup comp.

---
+++ src/resouces/api.controller.js
@@ -0,0 +1,28 @@
+'use strict'
+// 厳格モードを指定
+
+const db = require('../db/models/');
+
+const statusCode = {
+    OK: 200,
+    BAT: 400
+};
+
+module.exports = {
+    getTodos: (req, res) => {
+        send(res, statusCode.OK, 'getTodos', false);
+    },
+    postTodo: (req, res) => {
+        send(res, statusCode.OK, 'postTodo', false);
+    },
+    putTodo: (req, res) => {
+        send(res, statusCode.OK, 'putTodo', false);
+    },
+    deleteTodo: (req, res) => {
+        send(res, statusCode.OK, 'deleteTodo', false);
+    }
+};
+
+const send = (res, status, data, json = true) => {
+    res.status(status).send(json ? JSON.stringify(data) : data);
+};
---
+++ src/resouces/api.router.js
@@ -0,0 +1,15 @@
+'use strict'
+// 厳格モードを指定
+
+const express = require('express');
+const controller = require('./api.controller');
+const router = express.Router();
+
+router
+    .route('/todos')
+    .get(controller.getTodos)
+    .post(controller.postTodo)
+    .put(controller.putTodo)
+    .delete(controller.deleteTodo);
+
+module.exports = router;
